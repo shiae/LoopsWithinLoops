7064c99b558fdba53e8776b939986cdfffd317bb
Allison Shi <shiae@rose-hulman.edu>
2018-01-19 23:10:13 -0500

    m3 DONE: 6

--- src/m3_nested_loops_in_printing.py
+++ src/m3_nested_loops_in_printing.py
@@ -12,8 +12,8 @@ def main():
     # run_test_rectangle_of_stars()
     # run_test_triangle_of_stars()
     # run_test_decreasing_exclamation_marks()
-    run_test_alternating_brackets()
-    # run_test_triangle_same_number_in_each_row()
+    # run_test_alternating_brackets()
+    run_test_triangle_same_number_in_each_row()
     # run_test_triangle_all_numbers_in_each_row()
 
 
@@ -258,7 +258,7 @@ def triangle_same_number_in_each_row(r):
     Precondition:  r is a non-negative integer.
     """
     # ------------------------------------------------------------------
-    # TODO: 6. Implement and test this function.
+    # DONE: 6. Implement and test this function.
     #          Some tests are already written for you (above).
     #
     # IMPLEMENTATION RESTRICTION:
@@ -266,6 +266,11 @@ def triangle_same_number_in_each_row(r):
     #   in this or the other problems in this module, as doing so
     #   would defeat the goal of providing practice at loops within loops.
     # ------------------------------------------------------------------
+
+    for j in range(r):
+        for k in range(j + 1):
+            print(j + 1, end="")
+        print()
 
 
 def run_test_triangle_all_numbers_in_each_row():
